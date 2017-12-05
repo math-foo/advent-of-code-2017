47748f716ccc4db9260a93fc04b6b15e30402d5d
Caelyn McAulay <caelyn@ceruleanrodent.com>
2017-12-04 23:22:41 -0500

    Solutions to Day 4

---
+++ day_04_pt1/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::HashSet;
+
+fn main() {
+    let filename = "input"; 
+    let mut f = File::open(filename).expect("file not found");
+    
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    let mut valid_passphrases = 0;
+
+    for line in contents.lines() {
+        let total_word_count = line.split_whitespace().count();
+        let mut unique_words = HashSet::new();
+        for word in line.split_whitespace() {
+            unique_words.insert(word);
+        }
+        let unique_word_count = unique_words.len();
+        if unique_word_count == total_word_count {
+            valid_passphrases += 1;
+        }
+    }
+
+    println!("{}", valid_passphrases);
+}
---
+++ day_04_pt2/target/debug/day_04_pt2.d
@@ -0,0 +1,1 @@
+/home/caelyn/Projects/advent-of-code-2017/day_04_pt2/target/debug/day_04_pt2: /home/caelyn/Projects/advent-of-code-2017/day_04_pt2/src/main.rs
